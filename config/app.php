80853c6af7fc4988e78eba629a4db0770c4252c5
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2018-07-09 00:18:34 +0900

    require api-tester in this project

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Asvae\ApiTester\ServiceProvider::class,
 
         /*
          * Package Service Providers...
